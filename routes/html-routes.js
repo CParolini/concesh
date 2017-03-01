a352c7eed5eb257155d9ec4a8e0f4fb20e7fac3d
Dino Arachchi <Dino Arachchi>
2017-02-28 20:54:38 -0600

    Completed models, added additional seeds, and added basic GET routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,7 @@ var path = require("path");
 // Routes
 module.exports = function(app) {
 
-  // index route loads home.html
+  // Index route loads home.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
