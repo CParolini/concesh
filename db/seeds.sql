1399122317f68b489702049a5538fb49e5a35d6a
Dino Arachchi <Dino Arachchi>
2017-03-01 18:50:16 -0600

    Corrected database seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,20 @@
-USE concesh_db
+USE concesh_db;
 
-INSERT INTO menu_items (venue_name, vendor_name, item_name, item_price)
-VALUES ("Darrell K Royal-Texas Memorial Stadium", "Torchy's Tacos", "Trailer Park Taco", 4.50),
-("Darrell K Royal-Texas Memorial Stadium", "Torchy's Tacos", "Green Chile Pork Taco", 4.75),
-("Darrell K Royal-Texas Memorial Stadium", "Torchy's Tacos", "Green Chile Queso & Chips", 5.00),
-("Darrell K Royal-Texas Memorial Stadium", "Torchy's Tacos", "Chips & Salsa", 3.75);
+INSERT INTO venues (venue_name, latitude, longitude)
+VALUES ("Darrell K Royal-Texas Memorial Stadium", 30.2836813, -97.7325345);
+
+INSERT INTO vendors (vendor_name, venueId)
+VALUES ("La Cucina: Italian Cantina", 1),
+("Torchy's Tacos", 1);
+
+INSERT INTO menu_items (item_name, item_price, vendorId)
+VALUES ("Meatball Slider", 4.50, 1),
+("Italian Sausage", 5.75, 1),
+("Jumbo Dog", 5.25, 1),
+("Spinach & Feta Pretzel", 4.75, 1),
+("Texatalian Nachos", 5.25, 1),
+("Trailer Park Taco", 4.50, 2),
+("Green Chile Pork Taco", 4.75, 2),
+("Green Chile Queso & Chips", 5.00, 2),
+("Chips & Salsa", 3.75, 2);
+
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,7 @@ var path = require("path");
 
 // Routes
 module.exports = function(app) {
-
+  
   // Index route loads home.html
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
