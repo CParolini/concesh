5dc003726de1a87e54513786996919f5bc3ff480
Dino Arachchi <Dino Arachchi>
2017-03-04 11:17:10 -0600

    Reorganized files to correct folders

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,23 @@ var path = require("path");
 // Routes
 module.exports = function(app) {
   
-  // Index route loads home.html
-  app.get("/", function(req, res) {
+  // Home route loads home.html
+  app.get("/home", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
+  });
+
+  // Index route loads index.html
+  app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/index.html"));
+  });
+
+  // Thanks route loads thanks.html
+  app.get("/thanks", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/thanks.html"));
+  });
+
+  // UI route loads ui.html
+  app.get("/ui", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../public/ui.html"));
   });
 };
