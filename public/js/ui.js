0aef470a43738c50a639e5289fa64a36f1032414
Chris Parolini <cparolini@CParolini-MBP1.local>
2017-03-06 20:32:41 -0600

    adding a search page and cleaning up HTML

--- public/js/ui.js
+++ public/js/ui.js
@@ -51,15 +51,21 @@ function getMenus() {
 
 $(document).ready(function(){
 
+	$(".search-container").hide();
+	$(".welcome").remove();
+
 	// Initial login button click
 	$("#login").click(function() {
 		$(".welcome-container").hide();
+		$(".welcome").hide();
+		$(".search-container").hide();
 		$(".login-container").show();
 	});
 
 	// Initial signup button click
 	$("#signup").click(function() {
 		$(".welcome-container").hide();
+		$(".search-container").hide();
 		$(".regform-container").show();
 	});
 
@@ -67,10 +73,15 @@ $(document).ready(function(){
 	$(".submitCredentials").click(function() {
 		$(".regform-container").hide();
 		$(".login-container").hide();
-		$(".tix-info-container").show();
+		$(".search-container").show();
 
 		// Fill in dropdown menu for section/row/seat
 		getVenueInfo();
+	});
+
+	$(".search-submit").click(function(){
+		$(".search-container").hide();
+		$(".tix-info-container").show();
 	});
 
 	// Submit seat info and shows menus
@@ -85,7 +96,59 @@ $(document).ready(function(){
 	// Checkout and send payment
 	$("#checkout").click(function() {
 		$(".tix-info-container").hide();
-		$(".menu-container").hide()
+		$(".menu-container").hide();
 		$("#checkout-modal").show();
 	});
 });
+
+// Google autocomplete search that pulls information for the location that is chosen by the user. We will use the longitude and lattitude to locate the venue.
+		var placeSearch, autocomplete;
+		var componentForm = {
+				street_number: 'short_name',
+				route: 'long_name',
+				locality: 'long_name',
+				administrative_area_level_1: 'short_name',
+				country: 'long_name',
+				postal_code: 'short_name'
+		};
+		// console.log(autocomplete);
+
+		function initAutocomplete() {
+				// Create the autocomplete object, restricting the search to geographical
+				// location types.
+				autocomplete = new google.maps.places.Autocomplete(
+						/** @type {!HTMLInputElement} */
+						(document.getElementById('example-search-input')), {
+								types: ['geocode']
+						});
+
+				// When the user selects an address from the dropdown, populate the address
+				// fields in the form.
+				autocomplete.addListener('place_changed', fillInAddress);
+		}
+
+		function fillInAddress() {
+				// Get the place details from the autocomplete object.
+				var place = autocomplete.getPlace();
+
+				console.log((place.geometry.viewport.b.f + place.geometry.viewport.b.b)/2);
+				console.log((place.geometry.viewport.b.f + place.geometry.viewport.b.b)/2);
+		}
+
+		// Bias the autocomplete object to the user's geographical location,
+		// as supplied by the browser's 'navigator.geolocation' object.
+		function geolocate() {
+				if (navigator.geolocation) {
+						navigator.geolocation.getCurrentPosition(function(position) {
+								var geolocation = {
+										lat: position.coords.latitude,
+										lng: position.coords.longitude
+								};
+								var circle = new google.maps.Circle({
+										center: geolocation,
+										radius: position.coords.accuracy
+								});
+								autocomplete.setBounds(circle.getBounds());
+						});
+				}
+		}
