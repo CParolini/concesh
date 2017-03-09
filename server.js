41127725e752fff432b7e0d0ef073a0a5acece54
Dino Arachchi <Dino Arachchi>
2017-03-09 15:12:48 -0600

    Server is working with payment

--- server.js
+++ server.js
@@ -18,22 +18,27 @@ const charge = require('./charge');
 // create the server, and all the routes and configuration
 // go against this `app`
 const app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
+
+// Import routes and give the server access to them.
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // expose `process` to the view templates
 app.locals.process = process;
-
-// serve static assets
-app.use(express.static(path.join(__dirname, 'public')));
-
-// enable the body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// the router
-
-// GET /
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 // POST /charge
 app.post('/charge', (req, res, next) => {
@@ -44,7 +49,9 @@ app.post('/charge', (req, res, next) => {
   });
 });
 
-// start
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Listening');
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log("App listening on PORT " + PORT);
+	});
 });
